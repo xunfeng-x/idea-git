fcd7fd3757cff7888e558c9ee5cfe558be0fda5c
lifei <xunfeng_x@163.com>
2020-07-01 23:33:47 +0800

    2020/7/1从idea添加项目到git

---
+++ git-test/src/firstclass/FirstClass.java
@@ -0,0 +1,14 @@
+package firstclass;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+    public void demo(){
+        System.out.println("admin");
+    }
+}
